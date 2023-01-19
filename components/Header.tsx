927435dfb7c91a752539863244c4493289a9d3b3
mohanadkandil <mohanadmkandil@gmail.com>
2023-01-19 13:56:41 -0800

    chore: remove header

--- components/Header.tsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <header className="mt-5 flex w-full justify-center border-b-2 bg-transparent p-4 text-lg font-semibold">
-      <h1 className="text-4xl font-semibold">Linkedin Generator</h1>
-    </header>
-  );
-}
